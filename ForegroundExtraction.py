cb3172aec921eb144150c573a5bfe7dc332e13d2
Preston Price <pwprice57@gmail.com>
2017-02-11 21:26:46 -0800

    scaled contours to remove border

--- ForegroundExtraction.py
+++ ForegroundExtraction.py
@@ -8,16 +8,18 @@ from scipy import misc
 # blurred = cv2.GaussianBlur(img, (5, 5), 0)
 
 #fgmask = fgbg.apply(img)
-img = cv2.imread('BlueYellowJacket2.jpg')
-img = scipy.misc.imresize(img, 0.15)
+img = cv2.imread('TestImages/BlackShirt2.jpg')
+img = scipy.misc.imresize(img, 0.2)
 # apertureSize must be 1,3,5, or 7
-edgeImg = cv2.Canny(img, 100, 150, apertureSize=3)
+edgeImg = cv2.Canny(img, 100, 200, apertureSize=3)
 
-kernel = np.ones((5,5),np.uint8)
-# kernel = np.array([[]])
-edgeImg = cv2.dilate(edgeImg,kernel,iterations = 2)
-edgeImg = cv2.erode(edgeImg,kernel,iterations = 2)
-# closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+kernel_dilate = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+kernel_erode = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+
+edgeImg = cv2.dilate(edgeImg,kernel_dilate,iterations = 3 )
+edgeImg = cv2.erode(edgeImg,kernel_erode,iterations = 2)
+
+# # closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
 
 cv2.namedWindow('frame', flags=cv2.WINDOW_NORMAL)
 cv2.imshow('frame',edgeImg)
@@ -36,7 +38,7 @@ def findSignificantContours (img, edgeImg):
       tupl = np.insert(tupl, 0, [i])
       level1.append(tupl)
 
-     # From among them, find the contours with large surface area.
+  # From among them, find the contours with large surface area.
   significant = []
   tooSmall = edgeImg.size * 2 / 100 # If contour isn't covering 5% of total area of image then it probably is too small
   for tupl in level1:
@@ -50,17 +52,41 @@ def findSignificantContours (img, edgeImg):
 
   significant.sort(key=lambda x: x[1])
   #print ([x[1] for x in significant]);
-  return [x[0] for x in significant];
+  # compute the center of the contour
+  changeX, changeY = (0,0)
+
+  percent_scale = 90
+  c = [x[0] for x in significant]
+  for arr in c:
+    M = cv2.moments(np.array(arr))
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+
+    arr = arr*percent_scale/100
+    M2 = cv2.moments(np.array(arr))
+    cX2 = int(M2["m10"] / M2["m00"])
+    cY2 = int(M2["m01"] / M2["m00"])
+
+    changeX = cX-cX2
+    changeY = cY-cY2
+
+  
+
+  changes = [changeX, changeY]
+  significant = np.asarray(significant)
+  significant[0][0][:] = (significant[0][0][:]*percent_scale/100)+changes
+  print significant[0][0][:]
+  return [x[0] for x in significant]
 
 edgeImg_8u = np.asarray(edgeImg, np.uint8)
 
 # Find contours
 significant = findSignificantContours(img, edgeImg_8u)
-
+print significant[0][0][0]
 # Mask
 mask = edgeImg.copy()
 mask[mask > 0] = 0
-cv2.fillPoly(mask, significant, 255)
+cv2.fillPoly(mask, significant, 255, lineType=cv2.CV_AA)
 # Invert mask
 mask = np.logical_not(mask)
 
